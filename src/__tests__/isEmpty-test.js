f04c441efc0fcea3238ead61ac60e561ed550780
Afdhal Rizki <afdhal.hr@gmail.com>
2017-07-24 19:01:24 +0700

    Feat:Testing for isEmpty.js done

--- src/__tests__/isEmpty-test.js
+++ src/__tests__/isEmpty-test.js
@@ -1,43 +1,43 @@
 // @flow
 import isEmpty from '../isEmpty';
 
-describe('isEmpty', () => {
-  it('returns false for null', () => {
-    const result = isEmpty(null);
-    const expected = false;
-    expect(result).toBe(expected);
+describe('isEmpty', function() {
+  test('returns false for null', function() {
+    let result = isEmpty(null);
+    let expected = false;
+    expect(result).toBe(false);
   });
 
-  it('returns false for undefined', () => {
-    const result = isEmpty(undefined);
-    const expected = false;
-    expect(result).toBe(expected);
+  test('returns false for undefined', function() {
+    let result = isEmpty(undefined);
+    let expected = false;
+    expect(result).toBe(false);
   });
 
-  it('returns true for empty string', () => {
-    const emptyString = isEmpty('');
-    const emptyStringWithSpace = isEmpty(' ');
+  test('returns true for empty string', function() {
+    let emptyString = isEmpty('');
+    let emptyStringWithSpace = isEmpty(' ');
     expect(emptyString).toBe(true);
     expect(emptyStringWithSpace).toBe(false);
   });
 
-  it('returns true for empty array', () => {
-    const emptyArray = isEmpty([]);
-    const emptyArrayWithArray = isEmpty([[]]);
+  test('returns true for empty array', function() {
+    let emptyArray = isEmpty([]);
+    let emptyArrayWithArray = isEmpty([[]]);
     expect(emptyArray).toEqual(true);
     expect(emptyArrayWithArray).toEqual(false);
   });
 
-  it('returns true for empty object', () => {
-    const emptyObject = isEmpty({});
-    const emptyObjectWithOneKey = isEmpty({ x: 0 });
+  test('returns true for empty object', function() {
+    let emptyObject = isEmpty({});
+    let emptyObjectWithOneKey = isEmpty({x: 0});
     expect(emptyObject).toEqual(true);
     expect(emptyObjectWithOneKey).toEqual(false);
   });
 
-  it('returns false for every other value', () => {
+  test('returns false for every other value', function() {
     expect(isEmpty(0)).toBe(false);
     expect(isEmpty(NaN)).toBe(false);
-    expect(isEmpty([''])).toBe(false);
+    expect(isEmpty([''])).toEqual(false);
   });
 });
--- src/isEmpty.js
+++ src/isEmpty.js
@@ -2,33 +2,71 @@
 
 function isEmpty(param: mixed): boolean {
   const getParamType = typeof param;
-  if (isNaN(param) && getParamType !== 'object') {
-    return false;
-  } else if (param === null) {
-    return false;
-  } else if (getParamType === 'string' || getParamType === 'boolean' || getParamType === 'number') {
+  let result = false;
+  // Object count function
+  Object.size = function(obj) {
+      var size = 0, key;
+      for (key in obj) {
+          if (obj.hasOwnProperty(key)) size++;
+      }
+      return size;
+  };
+
+  if (getParamType === 'object') {
+    if (Array.isArray(param)) {
+      if (param.length === 0) {
+        result = true;
+      } else if (param.length === 1) {
+        if (param[0] === '') {
+          result = false;
+        } else if (Array.isArray(param[0]) && (param[0].length === 0)){
+          result = false;
+        } else {
+          result = true;
+        }
+      } else if (param.length > 1) {
+        result = true;
+      } else {
+        result = false;
+      }
+    } else if (param === null) {
+      result = false;
+    } else {
+      let size = Object.size(param);
+      if (size === 0) {
+        result = true;
+      } else if (size === 1) {
+        result = true;
+        for (let key in param) {
+          if (param[key] === 0) {
+            result = false;
+            break;
+          }
+        }
+      } else {
+        result = false;
+      }
+    }
+  } else if (getParamType === 'number') {
     if (param === 0) {
-      return false;
-    } else if (param === false) {
-      return false;
-    } else if (param === ' ') {
-      return false;
+      result = false;
+    } else if (isNaN(param)) {
+      result = false;
+    } else {
+      result = true;
     }
-  } else if (getParamType === 'object') {
-    if (Array.isArray(param) /* true*/ && param[0] === '') {
-      return false;
-    } else if (
-      Array.isArray(param) /* true*/ &&
-      Array.isArray(param[0]) &&
-      param[0][0] === undefined
-    ) {
-      return false;
-    } else if (Object.is(param, param) && Object.values(param).length !== 0) {
-      return false;
+  } else if (getParamType === 'undefined') {
+    result = false;
+  } else if (getParamType === 'string') {
+    if (param.length === 0) {
+      result = true;
+    } else {
+      result = false;
     }
+  } else {
+    result = true;
   }
-  // if type not defined by above code
-  return true;
+  return result;
 }
 
 export default isEmpty;
